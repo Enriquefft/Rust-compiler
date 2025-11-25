0b587c8dad1125d1714313dc701013b5a805e08b
Enriquefft <enriquefft2001@gmail.com>
2025-11-25 13:54:56 -0500

    chore: regular commit

--- build.zig
+++ build.zig
@@ -3,6 +3,33 @@ const std = @import("std");
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
-pub fn build(_: *std.Build) void {
-    // TODO
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "rust-compiler",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    if (b.args) |args| run_cmd.addArgs(args);
+
+    const run_step = b.step("run", "Run the rust-compiler");
+    run_step.dependOn(&run_cmd.step);
+
+    const unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const test_step = b.step("test", "Run unit tests and golden tests");
+    test_step.dependOn(&unit_tests.step);
+    // TODO: attach golden file runners once tests/ fixtures land per architecture roadmap.
+
+    // TODO: add documentation/auxiliary targets (e.g., docs, lint) when modules exist.
 }
