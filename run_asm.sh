b25f0aa102d8f777b8273225da4a2893d58a8a31
Enriquefft <enriquefft2001@gmail.com>
2025-11-26 12:36:13 -0500

    feat: removed warnings from .s runner

--- run_asm.sh
+++ run_asm.sh
@@ -14,12 +14,19 @@ if [ ! -f "$asm_path" ]; then
     exit 1
 fi
 
-cc_bin="${CC:-gcc}"
+cc_bin="${CC:-clang}"
 asm_abs=$(realpath "$asm_path")
 tmp_dir=$(mktemp -d)
 trap 'rm -rf "$tmp_dir"' EXIT
 
 exe_path="$tmp_dir/$(basename "${asm_abs%.s}")"
 
-"$cc_bin" -no-pie "$asm_abs" -o "$exe_path"
+extra_flags=()
+if "$cc_bin" --version 2>/dev/null | grep -qi "clang"; then
+    extra_flags+=("-Wno-everything")
+elif "$cc_bin" --version 2>/dev/null | grep -qi "gcc"; then
+    extra_flags+=("-w")
+fi
+
+"$cc_bin" -no-pie "${extra_flags[@]}" "$asm_abs" -o "$exe_path"
 "$exe_path" "$@"
